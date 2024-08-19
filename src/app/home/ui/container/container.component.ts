37b800324260b3bb47c32eec64eba0d0f0e9e2eb
anton.yakovenko <anton.yakovenko@one-inside.com>
2024-08-19 19:38:07 +0200

    added padding to container and fixed styles.css error

--- src/app/home/ui/container/container.component.ts
+++ src/app/home/ui/container/container.component.ts
@@ -4,7 +4,7 @@ import { Component, input, OnInit } from '@angular/core';
     standalone: true,
     selector: 'app-container',
     template: `
-        <div class="mx-auto max-w-fit overflow-auto px-4 m-6 mr-6 ml-6 drop-shadow-lg shadow-yellow-300 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col">
+        <div class="mx-auto max-w-fit overflow-auto p-15 px-4 m-6 mr-6 ml-6 drop-shadow-lg shadow-yellow-300 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col">
             <h1 class="mb-2 text-2xl font-bold tracking-tight text-green-500 dark:text-white">{{headerTitle()}}</h1>
             <ng-content></ng-content>
         </div>
