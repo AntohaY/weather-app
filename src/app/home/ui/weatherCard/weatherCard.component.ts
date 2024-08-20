0740340d29cfd7dc5231d65b11a139f6ba7647cd
anton.yakovenko <anton.yakovenko@one-inside.com>
2024-08-20 10:13:35 +0200

    changed design of weather card slightly

--- src/app/home/ui/weatherCard/weatherCard.component.ts
+++ src/app/home/ui/weatherCard/weatherCard.component.ts
@@ -1,5 +1,4 @@
 import { Component, computed, input } from '@angular/core';
-import { WeatherConditionsIcons, WeatherConditionsDescription } from '../../utils/enums';
 import { IconComponent } from '../icon/icon.component';
 import { DailyForecast } from '../../interfaces/weather';
 import { ParseDatePipe } from "../../../shared/pipes/date.pipe";
@@ -11,16 +10,18 @@ import { WindSpeedDescriptionPipe } from "../../../shared/pipes/windSpeed.pipe";
   imports: [IconComponent, ParseDatePipe, DatePipe, WindSpeedDescriptionPipe],
   selector: 'app-weather-card',
   template: `
-      <div class="max-w-3xl min-w-96 m-4 p-6 drop-shadow-lg shadow-yellow-300 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center">
-          <a href="#">
-        <h1 class="mb-2 text-2xl font-bold tracking-tight text-red-500 dark:text-white">{{ weatherInfo().date.toString() | parseDate | date:'fullDate' }}</h1>
+      <div class="weather-card max-w-96 min-w-96 m-4 p-6 drop-shadow-lg shadow-yellow-300 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center">
+        <a href="#">
+          <h1 class="mb-2 text-2xl font-bold tracking-tight text-red-500 dark:text-white">{{ weatherInfo().date.toString() | parseDate | date:'fullDate' }}</h1>
         </a>
-        <p class="mb-3 text-2xl font-bold text-yellow-500 dark:text-gray-400">
-          Max temperature {{ weatherInfo().temp2m.max }}° C
-        </p>
-        <p class="mb-3 text-2xl font-bold text-yellow-300 dark:text-gray-400">
-          Min temperature {{ weatherInfo().temp2m.min }}° C
-        </p>
+        <div class="flex">
+          <p class="m-5 text-2xl font-bold text-yellow-500 dark:text-gray-400">
+            Max {{ weatherInfo().temp2m.max }}°C
+          </p>
+          <p class="m-5 text-2xl font-bold text-yellow-300 dark:text-gray-400">
+            Min {{ weatherInfo().temp2m.min }}°C
+          </p>
+        </div>
         <div class="flex item-center flex-col">
           <p class="mb-3 text-2xl font-normal text-blue-600 dark:text-gray-400">
             Wind speed: {{ +weatherInfo().wind10m_max | windSpeedDescription}}
@@ -33,7 +34,14 @@ import { WindSpeedDescriptionPipe } from "../../../shared/pipes/windSpeed.pipe";
         </div>
         <app-icon [weatherCondition]="weatherInfo().weather"></app-icon>
       </div>
-  `
+  `,
+  styles: [
+    `
+      .weather-card {
+        min-height: 520px;
+      }
+    `
+  ]
 })
 
 export class WeatherCardComponent{
